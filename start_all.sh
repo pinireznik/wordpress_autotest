f331f28e43314aefcd3b638c9baa6c898d7821f5
preznik <preznik@piniubuntults.(none)>
2013-12-18 11:06:16 +0100

    change IPs

--- start_all.sh
+++ start_all.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+IPADDRESS=192.168.1.13
+
 docker run -name wp -d -p 49100:80 -p 22 preznik/wordpress
 
 sleep 20
 
-docker run -v `pwd`/selenium/:/tmp/src/ preznik/selenium-python xvfb-run python /tmp/src/setup-wordpress.py http://172.20.10.9:49100
+docker run -name=selenium1 -v `pwd`/selenium/:/tmp/src/ preznik/selenium-python xvfb-run python /tmp/src/setup-wordpress.py http://${IPADDRESS}:49100
 
 sleep 10
 
-docker run -v `pwd`/selenium/:/tmp/src/ preznik/selenium-python xvfb-run python /tmp/src/add-comment.py http://172.20.10.9:49100 sgdsfsdfsdfsdfgds
+docker run -name=selenium2 -v `pwd`/selenium/:/tmp/src/ preznik/selenium-python xvfb-run python /tmp/src/add-comment.py http://${IPADDRESS}:49100 sgdsfsdfsdfsdfgds
 
-#docker kill wp ; docker rm wp
+#docker kill wp selenium1 selenium2 ; docker rm wp selenium1 selenium2
 
